4b5f94a13d0dc631b20dd6c81485966b48dc8d83
Tarun Nair <tarunnair200029@gmail.com>
2021-01-21 11:10:27 +0530

    Added changes

--- Start.py
+++ Start.py
@@ -1,6 +1,10 @@
 from termcolor import colored
 import sys
-from getpass import getpass
+import os
+os.system('color')
+
+    
+
 
 print(colored('Hello, This is ', 'magenta'))
 print(colored('''    
@@ -19,16 +23,14 @@ print(colored("This is version 1.0 of Sleet",'green'))
 print(colored("Sleet is a bot and many websites do not allow bots so",'cyan'))
 print(colored("*winks in binary*",'green'))
 print(colored("I have a fix.",'cyan'))
-print("Disclaimer : Creator of this script is not responsible for any misuse and the user is using it at his own risk.\nThis application was made just for a fun project\nDo you want to continue (y/n):")
+print("Disclaimer : Creator of this script is not responsible for any misuse and the user is using it at his own risk and is responsible for any misuse done.\nThis application was made just for a fun project\nDo you want to continue (y/n):")
 cont = input()
 if cont != "y" :
     sys.exit("Wrong! bye bye ")
 
 
-print("Oh, still here ? atleast take me on a date first")
-print("alright alright, eh you're no fun\n")
 print("Ensure to type in correct details otherwise the bot won't run and \nmight fail and do something bad\nhe's a bit dumb or she, I don't know we don't discriminate")
 email = input("Enter your email-ID : ")
-print("Enter your password : ")
-
+password = input("Enter you password : ")
 print("Don't worry sleet does not save your details,\nyour credentials will be erased after you close the application")
+
